08c749e7544dd1dbc25f55f8201dafdcbac45ff8
Honesty <honestymoore77@gmail.com>
2023-03-10 10:53:00 -0600

    add routing

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import AuthPage from '../AuthPage/AuthPage';
 import GlobalPage from '../GlobalPage/GlobalPage';
 import CreateThread from '../CreateThread/CreateThread';
@@ -11,13 +11,28 @@ import { getUser } from '../../utilities/users-service'
 export default function App() {
   const [user, setUser] = useState(getUser())
 
+  function App() {
+	const [isAuthenticated, setIsAuthenticated] = useState(
+	  () => JSON.parse(localStorage.getItem('token')) || false
+	)
+  
+	const setAuth = (value) => {
+	  setIsAuthenticated(value)
+	  //alert(value);
+	}
+  
+	useEffect(()=>{
+	  localStorage.setItem("token", JSON.stringify(isAuthenticated))
+	}, [isAuthenticated])
+
  return (
 		<main className='App'>
 			{user ? (
 				<>
 					<NavBar user={user} setUser={setUser}/>
 					<Routes>
-						<Route path='/global' element={<GlobalPage />} />
+						<Route path='/global' element={isAuthenticated ? <GlobalPage /> : <Navigate to="/AuthPage" replace/>} />
+						<Route path="/login" element={<AuthPage/>} />
 						<Route path='/create' element={<CreateThread />} />
 					</Routes>
 				</>
@@ -26,4 +41,4 @@ export default function App() {
 			)}
 		</main>
 	)
-}
+}}
