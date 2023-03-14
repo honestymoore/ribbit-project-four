84a2ba4603a6a1ad886876da4c2e0c60b349ff82
Honesty <honestymoore77@gmail.com>
2023-03-14 17:50:26 -0500

    more deployment

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const cors = require('cors')
 const logger = require('morgan');
-// Always require and configure near the top
+
 require('dotenv').config();
-// Connect to the database
 require('./config/database');
 
 
@@ -14,30 +13,17 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-app.use(cors({ origin: process.env.PORT || `http://localhost:3000` }))
-
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:3000` }))
 
 app.use(require('./config/checkToken'))
 
 const port = process.env.PORT || 3001;
 
-// Put API routes here, before the 'catch all' route
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/thread', require('./routes/api/thread'))
 
 app.use('/api/comment', require('./routes/api/comment'))
-
-
-// The following 'catch all' route (note the *) is necessary
-// to return the index.html on all non-AJAX/API requests
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`);
